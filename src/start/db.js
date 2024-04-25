96437733856983f88858ed4b3887dc7b1c1949c5
akbarEdcrm <168091320+akbarEdcrm@users.noreply.github.com>
2024-04-25 19:19:10 +0500

    change database name

--- src/start/db.js
+++ src/start/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = function () {
-    mongoose.connect("mongodb+srv://akbarali:akbarali2206@cluster0.1t6ltwf.mongodb.net/live?retryWrites=true&w=majority")
+    mongoose.connect("mongodb+srv://akbarali:akbarali2206@cluster0.1t6ltwf.mongodb.net/educrm?retryWrites=true&w=majority")
         .then(() => {
             console.log("Connected to MongoDb...");
         })
